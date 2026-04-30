1270ae96a16ef63f9fcb02ef4d1c7de6c87cd451
Eliel_Guedes.07 <elielguedes817@gmail.com>
2026-04-30 17:59:58 -0300

    update

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,17 +1,16 @@
 from pydantic import BaseModel, Field, EmailStr
 from typing import Annotated
-from uuid import UUID
 
 class UserBase(BaseModel):
     name: str = Field(..., max_length=100)
     email: EmailStr
-    adm: bool = Field(default = False)
 
 class UserCreate(UserBase):
     senha: Annotated[str, Field(max_length = 100)]
 
 class UserResponse(BaseModel):
-    id: UUID
+    id: str
     name: str
     email: EmailStr
     adm: bool
+
